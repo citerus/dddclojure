bb666b0241ecf54d7cbf000f192ec1ee33de9afb
Patrik Fredriksson <patrik@snart.nu>
2011-04-14 21:36:01 +0200

    Set up project

---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject dddclojure "0.0.1-SNAPSHOT"
+  :description "DDD Clojure demo"
+  :dependencies [[org.clojure/clojure "1.2.1"]
+                 [org.clojure/clojure-contrib "1.2.0"]
+                 [congomongo "0.1.3-SNAPSHOT"]
+                 [midje "1.1"]])
