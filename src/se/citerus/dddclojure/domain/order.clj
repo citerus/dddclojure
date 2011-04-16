0954521ab23071376d9e05dd40d3e3d49503ab26
Patrik Fredriksson <patrik@snart.nu>
2011-04-16 23:55:34 +0200

    Changed data structure for line items to map.

--- src/se/citerus/dddclojure/domain/order.clj
+++ src/se/citerus/dddclojure/domain/order.clj
@@ -11,22 +11,26 @@
 (defn line-total [{:keys [qty piece-price]}]
   (* qty piece-price))
 
+(defn find-line-item [lines product]
+  (filter #(= (:product %) product) lines))
+
 (defrecord LineItem [product qty piece-price])
 
-(defrecord SimpleOrder [number date status limit lines]
+(defrecord SimpleOrder [number date status limit lines] ;Create FastOrder where LineItems are in a map
   Order
-
   (add-item [this product qty piece-price]
-    (update-in this [:lines] conj (LineItem. product qty piece-price)))
-
+    (update-in this [:lines] assoc product (LineItem. product qty piece-price)))
   (order-total [this]
-    (reduce #(+ %1 (line-total %2)) 0 (:lines this))))
+    (reduce #(+ %1 (line-total %2)) 0 (vals (:lines this))))
+  (remove-item [this product qty]
+    (update-in this [:lines product :qty] - qty))
+  )
 
 
 ;; Order factory methods
 
 (defn create-order [number date limit]
   ;User :pre condition to handle limit
-  (SimpleOrder. number date ::open limit []))
+  (SimpleOrder. number date ::open limit {}))
 
 
--- test/se/citerus/dddclojure/domain/order_test.clj
+++ test/se/citerus/dddclojure/domain/order_test.clj
@@ -1,6 +1,7 @@
 (ns se.citerus.dddclojure.domain.order-test
   (:use
-    [se.citerus.dddclojure.domain.order :only (create-order add-item order-total)]
+    [se.citerus.dddclojure.domain.order
+     :only (create-order add-item order-total remove-item)]
     [midje.sweet])
   (:require
     [se.citerus.dddclojure.domain.order :as o])
@@ -15,20 +16,38 @@
 
 (fact "Create order"
   (create-order 1 order-time-point 2000)
-  => (SimpleOrder. 1 order-time-point ::o/open 2000 []))
+  => (SimpleOrder. 1 order-time-point ::o/open 2000 {}))
 
 (facts "Order scenario"
 
   (fact "Create order"
-    (create-order 1 order-time-point 2000) =>
-    (SimpleOrder. 1 order-time-point ::o/open 2000 []))
+    (create-order 1 order-time-point 2000)
+    => (SimpleOrder. 1 order-time-point ::o/open 2000 {}))
 
-  (fact "Add one item"
-    (let [order (create-order 1 order-time-point 2000)
-          cheese-order (add-item order "Cheese" 2 10)]
-      cheese-order => (SimpleOrder. 1 order-time-point ::o/open 2000 [(LineItem. "Cheese" 2 10)])))
+  (let [order (create-order 1 order-time-point 2000)]
+    (fact "Add one item"
 
-  (fact "Calculate order total"
-    (let [order (create-order 1 order-time-point 2000)
-          order-with-items (-> order (add-item "Cheese" 2 10) (add-item "Ham" 1 20) (add-item "Juice" 2 15))]
-      (order-total order-with-items) => 70)))
+      (add-item order "Cheese" 2 10)
+      => (SimpleOrder. 1 order-time-point ::o/open 2000 {"Cheese" (LineItem. "Cheese" 2 10)}))
+
+    (fact "Calculate order total"
+      (let [order-with-items
+            (-> order
+              (add-item "Cheese" 2 10)
+              (add-item "Ham" 1 20)
+              (add-item "Juice" 2 15))]
+
+        (order-total order-with-items)
+        => 70))
+
+    (fact "Remove item from order"
+      (let [order-with-items
+            (-> order
+              (add-item "Cheese" 2 10)
+              (add-item "Ham" 1 20))]
+
+        (remove-item order-with-items "Cheese" 1)
+        =>
+        (SimpleOrder. 1 order-time-point ::o/open 2000
+          {"Cheese" (LineItem. "Cheese" 1 10)
+           "Ham" (LineItem. "Ham" 1 20)})))))
